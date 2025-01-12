98cca6badd21b0c56bab47634cb358b574a19a95
ayousef27 <ahmenero27@gmail.com>
2025-01-12 14:19:37 +0300

    P2

--- carcollector/main_app/models.py
+++ carcollector/main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Car(models.Model):
+    name = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    color = models.TextField(max_length=250)
+    production = models.IntegerField()
+    image = models.ImageField(upload_to='main_app/static/uploads/', default='')
